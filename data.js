cc32f6f711e963b1da1b39e84b05f97175917081
Raehale <87107302+Raehale@users.noreply.github.com>
2024-06-19 02:34:21 -0600

    adding pets array

---
+++ data.js
@@ -0,0 +1,315 @@
+export const petsData = [
+    //Arlo
+    {
+        imageTags: ["Arlo"],
+        isGif: false,
+        image: "Arlo-1.jpg",
+        alt: "A black and white cat laying in a humans lap.",
+        caption: "Arlo laying in Rae's Lap for the First Time.",
+        indexId: 1,
+    },
+    {
+        imageTags: ["Arlo"],
+        isGif: false,
+        image: "Arlo-2.jpg",
+        alt: "A black and white cat sitting on a porch with his mouth open.",
+        caption: "Arlo making a silly stinky face.",
+        indexId: 2,
+    },
+    {
+        imageTags: ["Arlo"],
+        isGif: false,
+        image: "Arlo-3.jpg",
+        alt: "A black and white cat sitting on a cut up log, and looking up into a tree.",
+        caption: "Arlo plotting a really big jump.",
+        indexId: 3,
+    },
+    {
+        imageTags: ["Arlo"],
+        isGif: false,
+        image: "Arlo-4.jpg",
+        alt: "A black and white cat looking at the camera while laying in front of the window.",
+        caption: "Arlo on his purch, being judgey.",
+        indexId: 4,
+    },
+    
+    //Darryl
+    {
+        imageTags: ["Darryl"],
+        isGif: false,
+        image: "Darryl-1.jpg",
+        alt: "A small black dog with white hairs looking at the camera.",
+        caption: "Darryl looking like he did something wrong (his normal face).",
+        indexId: 5,
+    },
+    
+    //Dolly
+    {
+        imageTags: ["Dolly"],
+        isGif: false,
+        image: "Dolly-1.jpg",
+        alt: "A white rat with red eyes and a pink nose poking their head out some blankets.",
+        caption: "Dolly begging for snacks.",
+        indexId: 6,
+    },
+    {
+        imageTags: ["Dolly"],
+        isGif: false,
+        image: "Dolly-2.jpg",
+        alt: "A white dumbo rat with red eyes and a pink nose poking their head out a UFO hide.",
+        caption: "Dolly is an alien #confirmed.",
+        indexId: 7,
+    },
+    {
+        imageTags: ["Dolly"],
+        isGif: false,
+        image: "Dolly-3.jpg",
+        alt: "W white dumbo rat with red eyes and a pink nose poking their head of a UFO Hide.",
+        caption: "She's sucha pretty girl.",
+        indexId: 8,
+    },
+    //Dolly and Toast
+    {
+        imageTags: ["Dolly", "Toast"],
+        isGif: false,
+        image: "Dolly-and-Toast-1.jpg",
+        alt: "Two white rats, one looking to the left, the other sqished behind looking out of their hide.",
+        caption: "I see you too Toasty.",
+        indexId: 9,
+    },
+    {
+        imageTags: ["Dolly", "Toast"],
+        isGif: false,
+        image: "Dolly-and-Toast-2.jpg",
+        alt: "Two rats hiding in a hide, on a pink cat tree, within a green blanket.",
+        caption: "Why do they always sleep like this?",
+        indexId: 10,
+    },
+    {
+        imageTags: ["Dolly", "Toast"],
+        isGif: false,
+        image: "Dolly-and-Toast-3.jpg",
+        alt: "Two white rats hiding in a UFO hide, poking their heads out.",
+        caption: "Dolly's fat arm and Toasters little nose awwe.",
+        indexId: 11,
+    },
+
+    //Etta and Dolly
+    {
+        imageTags: ["Etta", "Dolly"],
+        isGif: false,
+        image: "Etta-and-Dolly-1.jpg",
+        alt: "Four white rats in a small white wired cage, two looking at the camera.",
+        caption: "Etta getting extra cuddles after surgery.",
+        indexId: 12,
+    },
+    
+    //Jasper
+    {
+        imageTags: ["Jasper"],
+        isGif: false,
+        image: "Jasper-1.jpg",
+        alt: "A small black dog in the backseat of a car, laying in a brown dog carseat, looking at the camera.",
+        caption: "Jasper got sleepy on our drive to work.",
+        indexId: 13,
+    },
+    
+    //Jasper and Arlo
+    {
+        imageTags: ["Jasper", "Arlo"],
+        isGif: false,
+        image: "Jasper-2.jpg",
+        alt: "A black and white cat laying next to a small black dog, laying on a green couch.",
+        caption: "Arlo and Jasper sitting together for the first time.",
+        indexId: 14,
+    },
+    
+    //Mitski
+    {
+        imageTags: ["Mitski"],
+        isGif: false,
+        image: "Mitski-1.jpg",
+        alt: "A white rat with red eyes and a pink poking her head up from the basket she's in.",
+        caption: "Mitski saying hi.",
+        indexId: 15,
+    },
+    {
+        imageTags: ["Mitski"],
+        isGif: false,
+        image: "Mitski-2.jpg",
+        alt: "A white rat with red eyes and a pink nose stairing at the camera holding food.",
+        caption: "Mitski stole my cookie and won't give it back.",
+        indexId: 16,
+    },
+]
+
+// export const catsData = [
+//     {
+//         emotionTags: ["moody"],
+//         isGif: false,
+//         image: "angry.jpeg",
+//         alt: "A cat looking moody",
+//     },
+//     {
+//         emotionTags: ["moody", "insomniac"],
+//         isGif: false,
+//         image: "angry2.jpeg",
+//         alt: "A cat looking moody",
+//     },
+//     {
+//         emotionTags: ["moody"],
+//         isGif: false,
+//         image: "angry3.jpeg",
+//         alt: "A cat looking moody",
+//     },
+//     {
+//         emotionTags: ["confused", "sad"],
+//         isGif: false,
+//         image: "confused.jpeg",
+//         alt: "A cat looking confused",
+//     },
+//     {
+//         emotionTags: ["dominant", "moody"],
+//         isGif: false,
+//         image: "dominant.jpeg",
+//         alt: "A cat looking dominant",
+//     },
+//     {
+//         emotionTags: ["happy", "relaxed"],
+//         isGif: false,
+//         image: "happy.jpeg",
+//         alt: "A cat looking happy",
+//     },
+//     {
+//         emotionTags: ["hungry"],
+//         isGif: false,
+//         image: "hungry.jpeg",
+//         alt: "A cat looking hungry",
+//     },
+//     {
+//         emotionTags: ["hungry"],
+//         isGif: false,
+//         image: "hungry1.jpeg",
+//         alt: "A cat looking hungry",
+//     },
+//     {
+//         emotionTags: ["insomniac"],
+//         isGif: false,
+//         image: "insomnia.jpeg",
+//         alt: "A cat looking insomniac",
+//     },
+//     {
+//         emotionTags: ["insomniac"],
+//         isGif: false,
+//         image: "insomnia1.jpeg",
+//         alt: "A cat looking insomniac",
+//     },
+//     {
+//         emotionTags: ["relaxed"],
+//         isGif: false,
+//         image: "lazy.jpeg",
+//         alt: "A cat looking lazy",
+//     },
+//     {
+//         emotionTags: ["scared"],
+//         isGif: false,
+//         image: "nervous.jpeg",
+//         alt: "A cat looking nervous",
+//     },
+//     {
+//         emotionTags: ["sad"],
+//         isGif: false,
+//         image: "sad.jpeg",
+//         alt: "A cat looking sad",
+//     },
+//     {
+//         emotionTags: ["sad", "moody"],
+//         isGif: false,
+//         image: "sad1.jpeg",
+//         alt: "A cat looking sad",
+//     },
+//     {
+//         emotionTags: ["moody"],
+//         isGif: true,
+//         image: "angry.gif",
+//         alt: "A cat looking moody",
+//     },
+//     {
+//         emotionTags: ["moody"],
+//         isGif: true,
+//         image: "angry2.gif",
+//         alt: "A cat looking angry",
+//     },
+//     {
+//         emotionTags: ["confused"],
+//         isGif: true,
+//         image: "confused2.gif",
+//         alt: "A cat looking confused",
+//     },
+//     {
+//         emotionTags: ["dominant"],
+//         isGif: true,
+//         image: "dominant.gif",
+//         alt: "A cat looking dominant",
+//     },
+//     {
+//         emotionTags: ["happy"],
+//         isGif: true,
+//         image: "happy.gif",
+//         alt: "A cat looking happy",
+//     },
+//     {
+//         emotionTags: ["hungry", "sad", "confused"],
+//         isGif: true,
+//         image: "confused.gif",
+//         alt: "A cat looking hungry",
+//     },
+//     {
+//         emotionTags: ["hungry"],
+//         isGif: true,
+//         image: "hungry.gif",
+//         alt: "A cat looking hungry",
+//     },
+//     {
+//         emotionTags: ["hungry"],
+//         isGif: true,
+//         image: "hungry2.gif",
+//         alt: "A cat looking hungry",
+//     },
+//     {
+//         emotionTags: ["insomniac", "scared"],
+//         isGif: true,
+//         image: "insomnia2.gif",
+//         alt: "A cat looking insomniac",
+//     },
+//     {
+//         emotionTags: ["relaxed"],
+//         isGif: true,
+//         image: "lazy.gif",
+//         alt: "A cat looking relaxed",
+//     },
+//     {
+//         emotionTags: ["relaxed"],
+//         isGif: true,
+//         image: "relaxed2.gif",
+//         alt: "A cat looking relaxed",
+//     },
+//     {
+//         emotionTags: ["scared", "sad"],
+//         isGif: true,
+//         image: "nervous.gif",
+//         alt: "A cat looking nervous",
+//     },
+//     {
+//         emotionTags: ["scared"],
+//         isGif: true,
+//         image: "nervous2.gif",
+//         alt: "A cat looking scared",
+//     },
+//     {
+//         emotionTags: ["sad"],
+//         isGif: true,
+//         image: "sad.gif",
+//         alt: "A cat looking sad",
+//     },
+// ]
